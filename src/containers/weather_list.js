c03f9fb67cd1d17d1b849aa99353ce1fa530252d
ercekal <ercekalabalikoglu@gmail.com>
2016-12-12 17:54:49 +0000

    uses the Sparklines to add charts and lists the cities

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp -273 );
+    const press = cityData.list.map(weather => weather.main.pressure);
+    const humids = cityData.list.map(weather => weather.main.humidity);
+
+
+      return (
+        <tr key={name}>
+          <td>{name}</td>
+          <td><Chart data={temps} color="orange" units="C"/></td>
+          <td><Chart data={press} color="red" units="hPa"/></td>
+          <td><Chart data={humids} color="blue" units="%"/></td>
+        </tr>
+      );
+    }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
